9b6fa1544dbac9d71729c001b761023e2f960a20
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-03-31 17:32:56 +0300

    tema

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,17 @@ public class Main {
 	// write your code here
         System.out.println("Hello");
         System.out.println("Emilia");
+        System.out.println( "Rezultatul adunarii dintre 24 si 26 este:");
         System.out.println(24+26);
-        System.out.print(12/3);
-
+        System.out.println( "Rezultatul impartirii dintre 15 si 3 este:");
+        System.out.println(15/3);
+        System.out.println( "Rezultatul operatiunii -5 + (8 * 6) este:");
+        System.out.println(-5 + (8 * 6));
+        System.out.println( "Rezultatul operatiunii (55+9) % 9 este:");
+        System.out.println((55+9) % 9);
+        System.out.println( "Rezultatul operatiunii 20 + (-3*5 / 8) este:");
+        System.out.println(20 + (-3*5 / 8f));
+        System.out.println( "Rezultatul operatiunii 5 + 15 / 3 * 2 - 8 % 3 este:");
+        System.out.println(5 + (15 / 3 * 2) - (8 % 3));
     }
 }
