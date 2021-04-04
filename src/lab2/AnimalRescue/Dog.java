b32be5197591e81e51ed3699b1c0032bf2edc17f
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-04 14:39:15 +0300

    tema

--- src/lab2/AnimalRescue/Dog.java
+++ src/lab2/AnimalRescue/Dog.java
@@ -1,4 +1,23 @@
 package lab2.AnimalRescue;
 
 public class Dog {
+
+    String name = "Dylan";
+    int age = 3;
+    int health = 8;
+    int hungry_level = 6;
+    int happiness = 9;
+    String name_of_food = "Belo";
+    String favourite_activity = "being pet";
+
+    public void eat(){
+        System.out.println("eating dog food Belo");
+    }
+    public void bark(){
+        System.out.println("Hamham");
+    }
+
+    public void play(){
+        System.out.println("jumping");
+    }
 }
