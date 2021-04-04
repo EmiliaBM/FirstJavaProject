6513a917c855f504147c953da32644381ff3817d
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-04 14:51:06 +0300

    tema Modul10

--- src/lab2/AnimalRescue/Girl.java
+++ src/lab2/AnimalRescue/Girl.java
@@ -1,4 +1,20 @@
 package lab2.AnimalRescue;
 
 public class Girl {
+
+    String name = "Ela";
+    String eyes = "green";
+    String gender = "girl";
+    int age = 12;
+    Float money = 130.12f;
+
+    public void pets(){
+        System.out.println("petting the dog");
+    }
+    public void brushes(){
+        System.out.println("brushes the dog");
+    }
+    public void feeds(){
+        System.out.println("feeds the dog");
+    }
 }
