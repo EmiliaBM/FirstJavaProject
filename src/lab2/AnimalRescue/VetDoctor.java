e08fd55992ea8dc1d8ac04381b89cc50770f0c0e
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-04 15:00:09 +0300

    tema Modul10

--- src/lab2/AnimalRescue/VetDoctor.java
+++ src/lab2/AnimalRescue/VetDoctor.java
@@ -1,4 +1,15 @@
 package lab2.AnimalRescue;
 
 public class VetDoctor {
+
+    String name = "Mario";
+    String specialist = "Head of the Vet Clinic";
+
+    public void treats(){
+        System.out.println("shots an injection");
+    }
+
+    public void laughs(){
+        System.out.println(" Laughs at the dog's jumps");
+    }
 }
