dccb43f998f526272bb9b59d93c59b1e3fcc3ba0
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-04 11:38:23 +0300

    tema

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -4,20 +4,26 @@ public class Main {
     public static void main(String[] args) {
         int i = multiplication (3,5);
         int ii = divide (10,5);
+        float iii = divide (27,5f);
         System.out.println(i);
         System.out.println(ii);
+        System.out.println(iii);
     }
 
     public static int multiplication (int first,int second){
-        int multiplication = first * second;
-        return multiplication;
+        int result = first * second;
+        return result;
 
         }
 
-
     private static int divide ( int first, int second) {
-        int divide = first / second;
-        return divide;
+        int result = first / second;
+        return result;
+
+    }
+    private static float divide ( float first, float second) {
+        float result = first / second;
+        return result;
 
     }
 }
