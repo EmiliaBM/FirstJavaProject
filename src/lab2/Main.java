71252bed4c3398811e5345206a41abaf5c855eed
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-05 12:38:14 +0300

    tema Modul10

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -3,11 +3,37 @@ package lab2;
 public class Main {
     public static void main(String[] args) {
         int i = multiplication (3,5);
-        int ii = divide (10,5);
-        float iii = divide (27,5f);
+        int ii = divided (10,5);
+        float iii = divides (27,5f);
+        int iiii = average(2,5,5)/3;
+        int iiiii = sum(5,7);
+        int iiiiii = substract(12,5);
+        int vi = multiplication(2,3,4);
+        float vii = divide(12,5f);
         System.out.println(i);
+        System.out.println();
         System.out.println(ii);
+        System.out.println();
         System.out.println(iii);
+        System.out.println();
+        System.out.println(iiii);
+        System.out.println();
+        System.out.println(iiiii);
+        System.out.println();
+        System.out.println(iiiiii);
+        System.out.println();
+        System.out.println(vi);
+        System.out.println();
+        System.out.println(vii);
+        System.out.println();
+        PrintJavaEx3();
+        System.out.println();
+        PrintJavaRobo();
+        System.out.println();
+
+
+
+
     }
 
     public static int multiplication (int first,int second){
@@ -16,34 +42,34 @@ public class Main {
 
     }
 
-    public static int divide ( int first, int second) {
+    public static int divided ( int first, int second) {
         int result = first / second;
         return result;
 
     }
-    public static float divide ( float first, float second) {
+    public static float divides ( float first, float second) {
         float result = first / second;
         return result;
 
     }
 
     public static int sum (int first, int second) {
-        int result = int first + int second;
+        int result = first + second;
          return result;
     }
 
     public static int substract (int first, int second) {
-        int result = int first - int second;
+        int result = first - second;
         return result;
     }
 
     public static int multiplication (int first, int second, int third) {
-        int result = int first * int second * int third;
+        int result = first * second * third;
         return result;
     }
 
-    public static int divide (int first, int second) {
-        int result = int first / int second;
+    public static float divide (float first, float second) {
+        float result = first / second;
         return result;
     }
     public static void PrintJavaEx3() {
@@ -53,16 +79,16 @@ public class Main {
         System.out.println(    "  jj   a     a    v    a     a");
     }
     public static int average (int first, int second, int third) {
-        int result = int first + int second + int third;
+        int result = (first + second + third)/3;
         return result;
 
         }
     public static void PrintJavaRobo() {
-        System.out.println( "+"""""+");
+        System.out.println(" + ''' + ");
         System.out.println("[| o o |]");
         System.out.println(" |  ^  | ");
         System.out.println(" | '_' | ");
-        System.out.println(" +_____+ ")
+        System.out.println(" +_____+ ");
         
-        
+    }   
 }
