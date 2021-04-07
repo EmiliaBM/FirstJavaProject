0198b0fc24a64b5510646cbe7ed622e70d3737a7
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-04-07 22:33:13 +0300

    tema

--- src/lab3/LogicalOp.java
+++ src/lab3/LogicalOp.java
@@ -40,4 +40,96 @@ public class LogicalOp {
             return ("Got to try some more");
 
     }
+
+    //Creati o metoda de tip String, care sa primeasca un parametru de tip String,
+    // si un parametru de tip int. Daca textul primit ca si parametru este egal cu “FastTrackIT”,
+    // si numarul primit ca si parametru este mai mic sau egal cu 3, returnati textul si numarul,
+    // in ordinea asta. Daca textul nu este “FastTrackIT” si numarul este mai mare sau egal cu 4,
+    // returnati numarul si textul, in ordinea asta. Apelati metoda in main() pentru a verifica
+    // daca functioneaza.
+
+    public String Fast(String texts, int number){
+
+        if(texts.equals("FastTrackIt") && number <=3){
+        return (texts + number);
+    }
+        else if (texts.equals("FastTrackIt") && number >=4){
+        return (number + texts);
+    }
+        else{
+        return ("neither");
+    }
+    }
+
+    //  Creati o metoda de tip String, care sa primeasca un parametru de tip int.
+    //  Daca numarul primit este mai mare de 8, SAU egal cu 6, atunci sa se returneze
+    //  “The amount of snow this winter was(cm): “ si numarul. Daca nu, sa printeze
+    //  “The forecast snow is(cm):” si numarul. Apelati metoda in main() pentru a
+    //  verifica daca functioneaza.
+
+    public String Snow (int numbers){
+
+        if( numbers > 8 || numbers == 6){
+            return ("The amount of snow this winter was(cm):"+numbers);
+        }
+        else {
+            return ("The forecast snow is(cm):"+numbers);
+        }
+
+    }
+
+    // Creati o metoda de tip String, care sa primeasca un parametru de tip numar.
+    // Daca numarul e mai mare decat 3, SI nu egal cu 4, returnati “The number is
+    // greater than 3 and not equal to 4”. Daca numarul este egal cu 4, returnati
+    // “The number is equal to 4”, iar daca numarul este mai mic de 3 returnati
+    // “The number is lower than 3”. Apelati metoda in main() pentru a verifica
+    // daca functioneaza.
+
+    public String BiggerOrLower (int numb){
+
+        if(numb >3 && numb !=4){
+            return ("The number is greater than 3 and not equal to 4");
+        }
+        else if( numb == 4){
+            return ("The number is equal to 4");
+        }
+        else if( numb<3){
+            return ("The number is lower than 3");
+        }
+        else
+            return("");
+
+    }
+
+    // Creati o metoda care sa se numeasca isNumberEven. Metoda trebuie sa primeasca ca parametru un
+    // numar, si sa returneze adevarat sau fals. Daca numarul e par sa returneze true iar daca e impar
+    // sa returnese false. Apelati metoda in main() pentru a verifica daca functioneaza.
+
+    public boolean TrueOrFalse(int first){
+
+        if(first % 2 == 0){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //Creati o metoda care sa se numeasca isEligibleToVote. Metoda sa primeasca ca si parametru un numar,
+    // care sa reprezinte varsta, iar metoda sa iti zica daca daca persoana are drept de vot. Daca varsta
+    // e peste 18, sa se returneze true iar daca nu, sa returneze false. Apelati metoda in main() pentru a
+    // verifica daca functioneaza.
+
+    public boolean isEligibleToVote(int varsta){
+
+        if(varsta>18){
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+
+
+
 }
--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -8,11 +8,53 @@ public class Main {
         int x = op.checkBiggerNumber(2, 5);
         System.out.println(x + "este cel mai mare");
 
+        System.out.println();
+
         String text = op.EgualsFastTrackIt("FastAndFurious");
         System.out.println(text);
+
+        System.out.println();
+
+        String texts = "FasterThanYou";
+        int number = 5;
+        System.out.println(op.Fast(texts,number));
+
+        System.out.println();
+
+        int numbers = 10;
+        System.out.println(op.Snow(numbers));
+
+        System.out.println();
+
+        int numb = 11;
+        System.out.println(op.BiggerOrLower(numb));
+
+        System.out.println();
+
+        // Creati o metoda care primeste un numar ca si parametru si sa afiseze ce numar a primit.
+        // Folosind constructia Switch-Case, verificati ce numar s-a primit si afisati textul
+        // "The number is:  x !" unde x trebuie sa reprezinte numarul apasat. Pentru exemplul
+        // de fata sa nu se foloseasca concatenarea stringului "The number is:" cu numarul de
+        // la parametru, ci sa se scrie intreg textul cu tot cu numar pentru fiecare caz din Switch-Case.
+        // Apelati metoda in main() pentru a verifica daca functioneaza.
+
+
+        int first = 17;
+        System.out.println(op.TrueOrFalse(first));
+
+        System.out.println();
+
+        int varsta = 21;
+        System.out.println(op.isEligibleToVote(varsta));
+
+        System.out.println();
+
 
 
     }
 }
+
+
+
 
 
